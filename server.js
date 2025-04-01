7895a03af2584f084bf0223fc381f208ba69dcab
hey600 <zzeli0137@gmail.com>
2025-04-01 16:22:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,9 @@ app.use(express.json());
 let gameState = { players: [20137581,283086588,310475181,1246317443,151346278,83665149,3888076128,1977047445,2054639766,3177450285,104594292,21694635,617712672,744518605,4959248736,7293614438,658130642,6041260107,1254947521,2433211999,896315701,1897816342,110613208,1166802636,8031895652,42357700534305,138080016,3258168873,3027675324] };
 // Route for POST requests to check if UID exists
 app.post('/', (req, res) => {
-    res.send("got here"); // Fix typo
-
     const { uid } = req.body;
 
     if (uid) {
-        res.send("got here"); // Fix typo
         const uidNum = Number(uid);
         const exists = gameState.players.includes(uidNum);
         res.send(exists);
