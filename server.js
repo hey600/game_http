49a929e2affb4123f98130945bb6164e0bc37226
hey600 <zzeli0137@gmail.com>
2025-04-01 20:07:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // In-memory variable to store player states
-let gameState = { players: [20137581,283086588,310475181,1246317443,151346278,83665149,3888076128,1977047445,2054639766,3177450285,104594292,21694635,617712672,744518605,4959248736,7293614438,658130642,6041260107,1254947521,2433211999,896315701,1897816342,110613208,1166802636,8031895652,42357700534305,138080016,3258168873,3027675324] };
+let gameState = { players: [20137581, 283086588, 310475181, 1246317443, 151346278, 83665149, 3888076128, 1977047445, 2054639766, 3177450285, 104594292, 21694635, 617712672, 744518605, 4959248736, 7293614438, 658130642, 6041260107, 1254947521, 2433211999, 896315701, 1897816342, 110613208, 1166802636, 8031895652, 42357700534305, 138080016, 3258168873, 3027675324] };
+
 // Route for POST requests to check if UID exists
 app.post('/', (req, res) => {
     const { uid } = req.body;
@@ -16,6 +18,22 @@ app.post('/', (req, res) => {
         res.send(exists);
     } else {
         res.status(400).send(false);
+    }
+});
+
+app.get('/luau-script', async (req, res) => {
+    const rawUrl = 'https://raw.githubusercontent.com/hey600/game_http/refs/heads/main/MainServer.lua'; // The working raw URL
+    try {
+        const response = await fetch(rawUrl);
+        if (!response.ok) {
+            res.status(response.status).send('Error fetching Lua file');
+            return;
+        }
+        const luaCode = await response.text();
+        res.send(luaCode);
+    } catch (error) {
+        console.error('Error fetching Lua file:', error);
+        res.status(500).send('Error fetching Lua file');
     }
 });
 
